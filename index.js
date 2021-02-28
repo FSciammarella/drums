1e47652161bf2b56199691340af09bfe3a2a8af6
Felipe Sciammarella <fl.sciammarella@ufrj.br>
2021-02-27 22:39:25 -0300

    more ergonomic buttons + refactored callbacks + pressed style

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const samples = ["tom-1", "tom-2", "tom-3", "tom-4","crash","snare","kick-bass"];
-const btnList = document.querySelectorAll(".drum"); 
-for (let i =0; i<btnList.length; i++){
-    // const sample = new Audio("sounds/"+samples[i]+".mp3");
+const samples = ["tom-1", "tom-2", "tom-3", "tom-4", "crash", "snare", "kick-bass"];
+const buttons = "asdfjkl"
+const btnList = document.querySelectorAll(".drum");
+
+function buttonAnimation(btn) {
+    btn.classList.toggle("pressed");
+    setTimeout(function (btn) {
+        btn.classList.toggle("pressed")
+    }, 100, btn);
+}
+for (let i = 0; i < btnList.length; i++) {
     let btn = btnList[i];
-    btn.addEventListener("click",(e)=>{
-        const sample = new Audio("sounds/"+samples[i]+".mp3")
-        // alert(e.target.classList+" was clicked!")
-        sample.play()
+    btn.addEventListener("click", function (e) {
+        const sample = new Audio("sounds/" + samples[i] + ".mp3");
+        buttonAnimation(this);
+        sample.play();
     });
 }
-document.addEventListener("keypress",(e)=>{
-    document.querySelector("."+e.key).click();
-console.log("keypressed: +", e.key);
+document.addEventListener("keydown", (e) => {
+    if (buttons.includes(e.key)) {
+        document.querySelector("." + e.key).click();
+    }
 })
