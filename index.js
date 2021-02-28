cde2f129968a95fb70b82ea472f7fe614e62ffd9
Felipe Sciammarella <fl.sciammarella@ufrj.br>
2021-02-27 21:27:31 -0300

    Added keyboard eventListeners + mobile styling

---
+++ index.js
@@ -0,0 +1,15 @@
+const samples = ["tom-1", "tom-2", "tom-3", "tom-4","snare", "crash","kick-bass"];
+const btnList = document.querySelectorAll(".drum"); 
+for (let i =0; i<btnList.length; i++){
+    // const sample = new Audio("sounds/"+samples[i]+".mp3");
+    let btn = btnList[i];
+    btn.addEventListener("click",(e)=>{
+        const sample = new Audio("sounds/"+samples[i]+".mp3")
+        // alert(e.target.classList+" was clicked!")
+        sample.play()
+    });
+}
+document.addEventListener("keypress",(e)=>{
+    document.querySelector("."+e.key).click();
+console.log("keypressed: +", e.key);
+})
